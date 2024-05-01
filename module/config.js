863274d85fc87e392dc3dd1873c190423c4ae760
Antony MAILLOT <maillot.antony@orange.fr>
2024-05-01 09:44:12 +0200

    Arcanes

--- module/config.js
+++ module/config.js
@@ -6,11 +6,37 @@ LdC.sexe = {
     masculin: "Masculin"
 }
 
-LdC.signe = {
+LdC.signes = {
     griffe: "LdC.griffe",
     souffle: "LdC.souffle",
     ecaille: "LdC.ecaille",
     sang: "LdC.sang"
+}
+
+LdC.competences = {
+    aucun: "",
+    athletisme: "LdC.athletisme",
+    autorite: "LdC.autorite",
+    bagarre: "LdC.bagarre",
+    equitation: "LdC.equitation",
+    volonte: "LdC.volonte",
+    creation: "LdC.creation",
+    debrouille: "LdC.debrouille",
+    duperie: "LdC.duperie",
+    furtivite: "LdC.furtivite",
+    vigilance: "LdC.vigilance",
+    intrigue: "LdC.intrigue",
+    negoce: "LdC.negoce",
+    persuasion: "LdC.persuasion", 
+    seduction: "LdC.seduction",
+    strategie: "LdC.strategie",
+    erudition: "LdC.erudition",
+    investigation: "LdC.investigation",
+    medecine: "LdC.medecine",
+    technique: "LdC.technique",
+    tir: "LdC.tir",
+    occultisme: "LdC.occultisme",
+    escrime: "LdC.escrime"
 }
 
 LdC.ecole = {
