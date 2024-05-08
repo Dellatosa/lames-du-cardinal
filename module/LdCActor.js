bd831c5542c1d88de7e7a7d224f50f6c31926418
Antony MAILLOT <maillot.antony@orange.fr>
2024-05-08 21:23:28 +0200

    fonts

--- module/LdCActor.js
+++ module/LdCActor.js
@@ -12,7 +12,16 @@ export default class LdCActor extends Actor {
 
             for (let [key, carac] of Object.entries(data.caracteritiques)) {
                 if(carac.valeur > carac.max) { carac.valeur = carac.max; }
+
+                if(carac.signe == "griffe" || carac.signe == "souffle") {
+                    carac.couleur = "noir"
+                }
+                else if(carac.signe == "ecaille" || carac.signe == "sang") {
+                    carac.couleur = "rouge"
+                }
             }
+
+            console.log(data);
 
             for (let [key, comp] of Object.entries(data.competences)) {
                 if(comp.valeur > comp.max) { comp.valeur = comp.max; }
